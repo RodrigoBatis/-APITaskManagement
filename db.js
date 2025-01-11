6f14f1a78abf9880d9deb6079901382e83275c5e
RodrigoBatis <rodrigofreirebatista@gmail.com>
2025-01-10 21:54:01 -0300

    Alterado a dependência mysql pela mysql2 pelo fato do workbench  usa o método de autenticação caching_sha2_password por padrão

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
-import mysql from "mysql"
+import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
-   host: "localhost",
-   user: "root",
-   password: "12345678",
-   database: "taskmanagement"
-})
+  host: 'localhost',
+  user: 'root',
+  password: '12345678',
+  database: 'taskmanagement',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.message);
+    return;
+  }
+  console.log('Conexão com o banco de dados estabelecida com sucesso!');
+});
